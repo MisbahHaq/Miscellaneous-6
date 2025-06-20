27e106ea45dd5fde93d72876565518907248ef5f
Misbah Haque <misbahmuneer2003@gmail.com>
2025-06-20 12:01:18 +0500

    Update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const ease = "power4.inOut";
-})
+
+    document.querySelectorAll("a").forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            const href = link.getAttribute("href");
+        });
+    });
+});
